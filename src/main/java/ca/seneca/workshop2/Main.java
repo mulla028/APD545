b94f8f97dbebc6d6a1fedfb7db91770e38c61c6f
mulla028 <amullagaliev@myseneca.ca>
2024-10-07 19:56:01 -0400

    main file added

---
+++ src/main/java/ca/seneca/workshop2/Main.java
@@ -0,0 +1,9 @@
+package ca.seneca.workshop2;
+
+import ca.seneca.workshop2.controllers.OrderController;
+
+public class Main {
+    public static void main(String[] args) {
+        OrderController.main(args);
+    }
+}
