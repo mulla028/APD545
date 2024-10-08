9d3b9fb3f0c06a3ca7e693563eaa1dbe8cfe9cc1
mulla028 <amullagaliev@myseneca.ca>
2024-10-07 20:46:17 -0400

    Header added

--- src/main/java/ca/seneca/workshop2/views/OrderView.java
+++ src/main/java/ca/seneca/workshop2/views/OrderView.java
@@ -1,3 +1,15 @@
+/**********************************************
+ Workshop #2
+ Course:APD 545 - Semester 5
+ Last Name: Mullagaliev
+ First Name: Amir
+ ID: 128102225
+ Section:ZAA
+ This assignment represents my own work in accordance with Seneca Academic Policy.
+ Signature AM
+ Date:10.07.2024
+ **********************************************/
+
 package ca.seneca.workshop2.views;
 
 import ca.seneca.workshop2.models.Order;
